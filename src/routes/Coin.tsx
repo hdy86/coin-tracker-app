1b18815013752c7d3864f377e2d2711609ca2cee
hdy86 <dyhong86@naver.com>
2023-01-05 13:05:57 +0900

    coin page link

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,45 @@
-import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 
-interface RouteParams {
+const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15vh;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+const Loader = styled.div`
+  text-align: center;
+`;
+
+interface IRouteParams {
   coinId: string;
+}
+interface IRouteState {
+  name: string;
 }
 
 function Coin() {
-  const { coinId } = useParams<RouteParams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<IRouteState>();
 
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
